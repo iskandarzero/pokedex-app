0193281675c6f1168551fd0da609a947e0031a29
iskandarzero <riderzerotg@gmail.com>
2023-02-10 19:47:11 -0300

    feat: PokemonCard component

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="(pokemon, index) in pokemons" v-bind:key="index">
+    <img v-bind:src="pokemon.sprites.front_default" />
+    <h3>{{ pokemon.name }}</h3>
+    <p v-for="(type, index) in pokemon.types" v-bind:key="index">{{ type.type.name }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      pokemons: Object
+    }
+  }
+</script>
