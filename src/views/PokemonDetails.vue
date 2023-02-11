48ee89511251d9aa5030b00aad224efd317e542c
iskandarzero <riderzerotg@gmail.com>
2023-02-11 13:02:20 -0300

    style: PokemonDetails.vue

--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -1,14 +1,24 @@
 <template>
-  <h1 v-on:click="filtered" >{{ pokemon[0].name }}</h1>
-  <img :src="pokemon[0].sprites.front_default" />
-  <div v-for="(type, index) in pokemon[0].types" :key="index">
-    <h4>{{ type.type.name }}</h4>
+  <div class="detailsCard">
+    <div class="pokemonInfo">
+      <img :src="pokemon[0].sprites.front_default" />
+      <div class="pokemonMeasures">
+        <p>{{ `Altura - ${pokemon[0].height} m` }}</p>
+        <p>{{ `Peso - ${String(pokemon[0].weight).slice(0, -1)} kg` }}</p>
+      </div>
+    </div>
+    <h1 v-on:click="filtered" >{{ pokemon[0].name }}</h1>
+    <div class="types">
+      <h4 v-for="(type, index) in pokemon[0].types" :key="index">
+        {{ type.type.name }}
+      </h4>
+    </div>
+    <div>
+      <p v-for="(stat, index) in pokemon[0].stats" :key="index">
+        {{ `${stats[index]} - ${stat.base_stat}` }}
+      </p>
+    </div>
   </div>
-  <div v-for="(stat, index) in pokemon[0].stats" :key="index">
-    <p>{{ `${stats[index]} - ${stat.base_stat}` }}</p>
-  </div>
-  <p>{{ `Altura - ${pokemon[0].height} m` }}</p>
-  <p>{{ `Peso - ${String(pokemon[0].weight).slice(0, -1)} kg` }}</p>
 </template>
   
 <script>
@@ -33,3 +43,28 @@ import { storeToRefs } from 'pinia'
     }
   }
 </script>
+
+<style lang="scss">
+  .detailsCard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .types {
+      display: flex;
+      h4 {
+        margin: 0 10px 15px;
+      }
+    }
+    h1 {
+      margin: 0;
+    }
+    .pokemonInfo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 250px;
+      }
+    }
+  }
+</style>
