4a50da21a9be14655b83913c5ffa2b0a8ad9ae77
iskandarzero <riderzerotg@gmail.com>
2023-02-11 13:03:33 -0300

    feat: NavBar component and stylization

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="/imgs/pokedex_logo.png" alt="pokedex logo">
+    </router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style lang="scss">
+  nav {
+    background-color: #DC0A2F;
+    display: flex;
+    img {
+      display: flex;
+      margin: 10px 0px 7px 15px;
+      width: 200px;
+    }
+  }
+</style>
