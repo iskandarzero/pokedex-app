491f4d12eceb98a5275e7264b9203f37a220a295
iskandarzero <riderzerotg@gmail.com>
2023-02-11 13:19:10 -0300

    fix: sass loader

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  css:        {
+  css:  {
     loaderOptions: {
       sass: {
         prependData: `
