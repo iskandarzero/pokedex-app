8e307d477e5b6aac9c34b58933f4e3c09661ecba
iskandarzero <riderzerotg@gmail.com>
2023-02-11 13:01:43 -0300

    style: SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <p>
+  <div class="searchContainer">
     <input placeholder="Bulbasaur" type="search" v-model="searchParam" />
-  </p>
-  <button v-on:click="searchPokemon" type="submit">Pesquisar</button>
-  <div>
-    <PokemonCard :pokemons="pokemons" />
+    <button v-on:click="searchPokemon" type="submit">Pesquisar</button>
+    <div class="cardsContainer">
+      <PokemonCard :pokemons="pokemons" />
+    </div>
   </div>
 </template>
 
@@ -59,3 +59,38 @@
     }
   }
 </script>
+
+<style lang="scss">
+  .searchContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    input {
+      color: #333;
+      font-size: 1.2rem;
+      margin-bottom: 20px;
+      padding: 1.5rem 2rem;
+      border-radius: 0.2rem;
+      background-color: rgb(255, 255, 255);
+      width: 23%;
+    }
+    button {
+      background-color:#DC0A2F; /* Green */
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+    }
+    .cardsContainer {
+      width: 40%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+  }
+</style>
