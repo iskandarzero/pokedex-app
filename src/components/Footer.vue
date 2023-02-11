0802d708cca582466c1cca5c322ded277d5a9ac4
iskandarzero <riderzerotg@gmail.com>
2023-02-11 13:03:20 -0300

    feat: Footer component and stylization

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>Alimentado pela PokeAPI v2</p>
+  </footer>
+</template>
+  
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+  
+<style lang="scss">
+  footer {
+    background-color: #DC0A2F;
+    position: fixed;
+    height: 25px;
+    width: 100%;
+    bottom: 0;
+    p {
+      display: flex;
+      justify-content: center;
+      margin: 0;
+      color: white;
+    }
+  }
+</style>
